cf7ff0fe0cb604f725d9db026c87c335a185a4b0
Martin Kenny <git@mkenny.net>
2025-01-28 19:51:37 +1030

    Simplified lipo command-lines.

--- opus-xcframework.sh
+++ opus-xcframework.sh
@@ -6,9 +6,9 @@ mkdir -p build/opus_macos
 mkdir -p build/opus_ios-simulator
 mkdir -p build/opus_tvos-simulator
 
-lipo build/opus_macos_arm64/lib/libopus.a build/opus_macos_x86_64/lib/libopus.a -create -output build/opus_macos/libopus.a
-lipo build/opus_ios-simulator_arm64/lib/libopus.a build/opus_ios-simulator_x86_64/lib/libopus.a -create -output build/opus_ios-simulator/libopus.a
-lipo build/opus_tvos-simulator_arm64/lib/libopus.a build/opus_tvos-simulator_x86_64/lib/libopus.a -create -output build/opus_tvos-simulator/libopus.a
+lipo build/opus_macos_*/lib/libopus.a -create -output build/opus_macos/libopus.a
+lipo build/opus_ios-simulator_*/lib/libopus.a -create -output build/opus_ios-simulator/libopus.a
+lipo build/opus_tvos-simulator_*/lib/libopus.a -create -output build/opus_tvos-simulator/libopus.a
 
 rm -rf build/xcframeworks/Opus.xcframework
 xcodebuild -create-xcframework \
