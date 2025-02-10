9b1fcb9da0903e5fc61f991b31182c9c97edf859
Martin Kenny <git@mkenny.net>
2025-02-10 12:26:44 +1030

    Sign the XCFrameworks.

--- xcframeworks-apple.sh
+++ xcframeworks-apple.sh
@@ -6,4 +6,8 @@ mkdir -p xcframeworks
 ./FFmpeg-xcframework.sh
 ./opus-xcframework.sh
 
+codesign --timestamp -s "Apple Development" xcframeworks/SDL2.xcframework
+codesign --timestamp -s "Apple Development" xcframeworks/FFmpeg.xcframework
+codesign --timestamp -s "Apple Development" xcframeworks/opus.xcframework
+
 7z a moonlight-apple-xcframeworks.zip ./xcframeworks/*
